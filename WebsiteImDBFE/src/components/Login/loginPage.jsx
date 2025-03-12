300f603a080cfad40449802daf93a9432e0e973f
Chau Ngoc Hoang Nam <namchau2003@gmail.com>
2025-03-12 15:45:42 +0700

    fix

--- WebsiteImDBFE/src/components/Login/loginPage.jsx
+++ WebsiteImDBFE/src/components/Login/loginPage.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Navbar from '../Navbar/Navbar'
-import { useNavigate, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 const LoginPage = () => {
   const [formData, setFormData] = useState({
