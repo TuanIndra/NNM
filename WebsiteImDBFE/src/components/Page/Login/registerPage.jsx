4258db369c53c290c0d0721545365fcd35fc2fc6
Vu Dang Khoa <vukhang.51189@gmail.com>
2025-03-18 09:35:09 +0700

    dieu chinh folder

--- WebsiteImDBFE/src/components/Page/Login/registerPage.jsx
+++ WebsiteImDBFE/src/components/Page/Login/registerPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Navbar from "../Navbar/Navbar";
+import Navbar from "../../Navbar/Navbar";
 
 export default function RegisterForm() {
   const [formData, setFormData] = useState({
